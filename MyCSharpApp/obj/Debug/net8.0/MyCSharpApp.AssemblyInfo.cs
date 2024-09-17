64ae10469639f23e4f2e2f06e1ae5df972c74905
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-09-17 17:17:27 +0100

    internship

--- MyCSharpApp/obj/Debug/net8.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net8.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9fa54b14b495e4797fdf5da437b61b156a0fcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
